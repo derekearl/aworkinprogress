a737f70973cbfa33149916e4b2fa568e17a848c8
Logan Ray <104516932+loganray48@users.noreply.github.com>
2023-02-09 13:00:37 -0700

    Add files via upload

---
+++ ScriptureMemorize/word.cs
@@ -0,0 +1,10 @@
+private class Word
+{
+    private static System.Random random = new System.Random();
+
+    private static int GenerateRandomNumber()
+    {
+        return random.Next(2, 6);
+    }
+}
+
