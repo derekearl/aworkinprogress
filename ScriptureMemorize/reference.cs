1762ea1c974e7cf1a26a1d5804510ae56e4c1846
derekearl <104701590+derekearl@users.noreply.github.com>
2023-02-14 15:07:46 -0700

    just kidding this the current update

--- ScriptureMemorize/reference.cs
+++ ScriptureMemorize/reference.cs
@@ -1,38 +1,14 @@
-using System;
+// using System;
 
-public class Reference
-{
-    private string _wipbook;
-    private int _wipchapter;
-    private int _wipverse;
-    private int _wipendVerse;
+// class deReference
+// {
+//     private string book;
+//     private string chapter;
+//     private string verse;
 
-    public Reference()
-    {
-        _wipbook = "2 Nephi";
-        _wipchapter = 2;
-        _wipverse = 24;
-        _wipendVerse = 25;
-    }
+// }
 
-    public string getBook() 
-    {
-        return _wipbook;
-    }
+// public DeReference (book, chapter, verse)
+// {
 
-    public int getChapter()
-    {
-        return _wipchapter;
-    }
-
-    public int getVerse()
-    {
-        return _wipverse;
-    }
-
-    public int getEndVerse()
-    {
-        return _wipendVerse;
-    }
-}
-
+// }
