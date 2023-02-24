5645d81d2752e73491d8afd120dcffc0584071dc
cwilkey4 <106196478+cwilkey4@users.noreply.github.com>
2023-02-24 15:19:03 -0700

    Add files via upload

--- Mindfulness/Activity.cs
+++ Mindfulness/Activity.cs
@@ -1,59 +1,103 @@
 using System;
 // This is the parent class. All of the child classes will use attributes and methods from this class.
 
-public class Activity
+namespace mindfulness
 {
-    // We need to decide if we want these to be protected or private.
-    // If protected, it will be open to all children classes automatically.
-    // If private, we will need getters and setters.
-    
-    private string _activityName;
-    private string _description; 
-
-    public Activity(string activityName, string description)
+    public class Activity
     {
-        _activityName = activityName;
-        _description = description;
-    }
+        // We need to decide if we want these to be protected or private.
+        // If protected, it will be open to all children classes automatically.
+        // If private, we will need getters and setters.
+        
+        private string _activityName = "";
+        private string _description = ""; 
+        protected int _duration;
 
-    // Activity activity1 = new Activity();
-
-    private string startMessage = $"Welcome to the {activityName} activity!";  
-
-    protected int duration; // We are going to prompt the user for this number. It may need to be converted (milliseconds and seconds)
-    
-    private string endMessage = $"Well done! You've finished {duration} seconds of the {activityName} activity";
-
-    // Methods!
-    // countDown()
-
-    public void spinner() // We need it to take a "duration" variable in place of timer
-    {
-        // For now, the spinner runs for a set time of 4 seconds. We may need to adapt that.
-        DateTime startTime = DateTime.Now;
-        TimeSpan timer = TimeSpan.FromSeconds(4); 
-
-        while (DateTime.Now - startTime < timer)
+        public string startMessage()
         {
-            Console.Write("|");
-            Thread.Sleep(500);
-
-            Console.Write("\b \b"); 
-            Console.Write("/"); 
-
-            Thread.Sleep(500);
-
-            Console.Write("\b \b"); 
-            Console.Write("|"); 
-
-            Thread.Sleep(500);
-
-            Console.Write("\b \b"); 
-            Console.Write("\\"); 
-
-            Thread.Sleep(500);
-            Console.Write("\b \b"); 
+            return $"Welcome to the {_activityName}! {_description}";
         }
-    }
 
+        public string GetActivityName()
+        {
+            return _activityName;
+        }
+
+        public void SetActivityName(string activityName)
+        {
+            _activityName = activityName;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+        
+        public int GetDuration()
+        {
+            return _duration;
+        }
+
+        public void SetDuration(int duration) // We are going to prompt the user for this number. It may need to be converted. (Seconds, Milliseconds)
+        {
+            _duration = duration;
+        }
+
+        public string endMessage()
+        {
+            return $"Well done! You've finished {_duration} seconds of the {_activityName} activity";
+        }
+
+        // Methods!
+        // public void countDown(int duration)
+        // {
+        //     DateTime startTime = DateTime.Now;
+        //     TimeSpan timer = TimeSpan.FromSeconds(duration);
+
+        //     while (DateTime.Now - startTime < duration)
+        //     {
+        //         // Run Activity
+        //     }
+        // }
+
+        public void spinner(int seconds) 
+        {
+            // DateTime startTime = DateTime.Now;
+            // TimeSpan timer = TimeSpan.FromSeconds(seconds); 
+
+            while (seconds > 0)
+            {
+                Console.Write("|");
+                Thread.Sleep(250);
+
+                Console.Write("\b \b"); 
+                Console.Write("/"); 
+
+                Thread.Sleep(250);
+
+                Console.Write("\b \b"); 
+                Console.Write("|"); 
+
+                Thread.Sleep(250);
+
+                Console.Write("\b \b"); 
+                Console.Write("\\"); 
+
+                Thread.Sleep(250);
+                Console.Write("\b \b"); 
+
+                seconds -= seconds;
+            }
+
+            // while (DateTime.Now - startTime < timer)
+            // {
+            // }
+        }
+
+    }
 }
