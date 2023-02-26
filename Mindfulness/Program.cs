04373d5e68210b01e8818c0ec8897af91762c968
suryiavishnusoranam <104380619+suryiavishnusoranam@users.noreply.github.com>
2023-02-25 19:05:58 -0700

    Program.cs update 2

--- Mindfulness/Program.cs
+++ Mindfulness/Program.cs
@@ -22,17 +22,17 @@ namespace Develop04
             string userSelection = Console.ReadLine();
 
             // Activities
-            BreathingActivity breathing = new BreathingActivity();
+            Activity breathing = new Activity();
             breathing.SetActivityName("Breathing Activity");
             breathing.SetDescription("For this exercise, you will measure your breathing.");
 
-            Activity activity2 = new Activity();
-            activity2.SetActivityName("Reflecting Activity");
-            activity2.SetDescription("For this exercise, you will be given a prompt to reflect upon.");
+            Activity reflection = new Activity();
+            reflection.SetActivityName("Reflecting Activity");
+            reflection.SetDescription("For this exercise, you will be given a prompt to reflect upon.");
 
-            Activity activity3 = new Activity();
-            activity3.SetActivityName("Listing Activity");
-            activity3.SetDescription("For this exercise, you will list as many responses as you can to the following prompt and question.");
+            Activity listing = new Activity();
+            listing.SetActivityName("Listing Activity");
+            listing.SetDescription("For this exercise, you will list as many responses as you can to the following prompt and question.");
 
 
             if (userSelection == "1")
@@ -50,29 +50,29 @@ namespace Develop04
             }
             else if (userSelection == "2")
             {
-                Console.WriteLine(activity2.StartMessage());
+                Console.WriteLine(reflection.StartMessage());
 
                 Console.WriteLine("How long, in seconds, would you like your session to be?");
                 string userDuration = Console.ReadLine();
                 int duration = int.Parse(userDuration);
-                activity2.SetDuration(duration);
+                reflection.SetDuration(duration);
 
                 // Do Activity
 
-                activity2.EndMessage();
+                reflection.EndMessage();
             }
             else if (userSelection == "3")
             {
-                Console.WriteLine(activity3.StartMessage());
+                Console.WriteLine(listing.StartMessage());
                 
                 Console.WriteLine("How long, in seconds, would you like your session to be?");
                 string userDuration = Console.ReadLine();
                 int duration = int.Parse(userDuration);
-                activity3.SetDuration(duration);
+                listing.SetDuration(duration);
 
                 // Do Activity
 
-                activity3.EndMessage();
+                listing.EndMessage();
             }
             else
             {
