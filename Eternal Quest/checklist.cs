2d5114f5a6e69defa3deb695500a4661ee990dda
cwilkey4 <106196478+cwilkey4@users.noreply.github.com>
2023-03-09 13:15:49 -0700

    Update checklist.cs

---
+++ Eternal Quest/checklist.cs
@@ -0,0 +1,26 @@
+using System;
+
+class deCheckList
+{
+
+
+
+
+private string _goalname;
+
+private string _goalDescription;
+
+private int _goalPoints;
+
+public deCheckList(string goalname, string goalDescription, int goalPoints)
+{
+    _goalname = goalname;
+    _goalDescription = goalDescription;
+    _goalPoints = goalPoints;
+}
+public void getGoal()
+{
+   
+}
+
+}
