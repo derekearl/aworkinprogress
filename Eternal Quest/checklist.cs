306d4e37e07be3fbf4ef86c382fc3dde60020a17
derekearl <104701590+derekearl@users.noreply.github.com>
2023-03-10 16:24:09 -0700

    Update checklist.cs

--- Eternal Quest/checklist.cs
+++ Eternal Quest/checklist.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 class deCheckList
 {
@@ -17,10 +18,41 @@ public deCheckList(string goalname, string goalDescription, int goalPoints)
     _goalname = goalname;
     _goalDescription = goalDescription;
     _goalPoints = goalPoints;
+    Console.WriteLine ("Welcome to the Checklist!");
+    Console.WriteLine ("Please select one of the following!");
 }
-public void getGoal()
+
+public static void goalList()
 {
-   
+    string entryList = "";
+
+
+while(entryList!="4"){
+    var displayList = new List<string>(3);
+    {
+        displayList.Add ("1. Show Goal");
+        displayList.Add ("2. Show Description");
+        displayList.Add ("1. Show Points");
+
+        for(int i=0;i<displayList.Count;i++)
+        {
+            Console.WriteLine(displayList[i]);
+        }
+    }
+    entryList = Console.ReadLine();
+    if(entryList == "1"){
+        Console.ReadLine(goalName);
+    }
+    else if(entryList == "2"){
+        Console.ReadLine(goalDescription);
+    }
+    else if(entryList == "3"){
+        Console.ReadLine(goalPoints);
+    }
 }
+
+}
+
+
 
 }
