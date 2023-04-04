d14aa041cc868d30068c34f24ec0cb063a9a52c4
cwilkey4 <106196478+cwilkey4@users.noreply.github.com>
2023-04-03 19:40:31 -0600

    Minor Changes

--- Eternal Quest/Goal.cs
+++ Eternal Quest/Goal.cs
@@ -4,53 +4,148 @@ namespace Develop05
 {
     public abstract class Goal
     {
-        private string _GoalName = "";
-        private string _Description = "";
-        private int _GoalPoints;
-        private int _TotalPoints;
-        private bool _isCompleted;
+        private string GoalName = "";
+        private string Description = "";
+        private int GoalPoints;
+        private int TotalPoints;
+        private int PointsEarned;
+        private bool isCompleted;
 
-        // Constructor. This function is called upon the creation of a goal.
         public string StartMessage()
         {
-            return $"Your goal is {_GoalName} and its description is {_Description}";
+            return $"Your goal is {GoalName} and its description is {Description}";
         }
-        public string GetDescription()
+        protected virtual int AddPoints(int pointsEarned, int goalPoints)
         {
-            return _Description;
+            int totalPoints = pointsEarned + goalPoints;
+            return totalPoints;
         }
 
-        public string GetGoalName()
+        /*
+        RecordEvent(): 
+            * Update goals that are listed in the file
+            * If simple goal, mark a goal is completed 
+            * If eternal goal, it only adds points
+            * If checklist goal, increment the number of completions add add points; also
+                check if the goal is entirely complete and add bonus points if yes
+        */
+        protected abstract int RecordEvent(string fileName, string userGoal);
+        // {
+        //     // We need to retrieve whatever goal the user has accomplished from a number
+        //     string[] lines = System.IO.File.ReadAllLines(fileName);
+
+        //     foreach (string line in lines)
+        //     {   
+        //         // Don't forget to skip the first line -- that contains the total points!
+        //         string[] parts = line.Split(",");
+
+        //         string goalType = parts[0];
+        //         string goalName = parts[1];
+        //         string goalDescription = parts[2];
+        //         string points = parts[3];
+        //         // string bonusPoints = parts[4]; (Checklist only!!) 
+                
+        //         int goalPoints = int.Parse(points);
+        //     }
+        //     // Then we are going to get the total amount of points from it
+
+        //     return goalPoints;
+        // }
+
+        //     if (!isCompleted)
+        //     {
+        //         if (isCompleted)
+        //         {
+        //             PointsEarned += GoalPoints;
+        //             return GoalPoints;
+        //         }
+        //         return 0;
+        //     }
+        //     return 0;
+        // }
+
+        protected abstract string IsCompleted(bool isCompleted);
         {
-            return _GoalName;
+            // We will also remember how many times this goal has been completed, if necessary. 
+            // (This will be specified in child classes)
+            // +1 (0/10 --> 1/10)
+            // if (isCompleted == false)
+            // {
+            //     string complete = "[ ]";
+            //     return complete;
+            // }
+            // else
+            // {
+            //     string complete = "[x]"; 
+            //     return complete;
+            // }
+
+            // ETERNAL WILL NOT UTILIZE THIS ONE
+            // Checklist will. It will not be boolean. It will need to be overriden.
         }
 
-        public int GetGoalPoints()
-        {
-            return _GoalPoints;
-        }
-        
-        public void SetGoalName(string GoalName)
-        {
-            _GoalName = GoalName;
-        }
-        
-        public void SetDescription(string Description)
-        {
-            _Description = Description;
-        }
-        
-        public void SetGoalPoints(int GoalPoints)
-        {
-            _GoalPoints = GoalPoints;
-        }
-        public abstract int AddPoints(int pointsEarned, int totalPoints);
-        
-        public virtual int RecordEvent(string fileName)
-        {
-            if (!_isCompleted) {return 0;}
-            return _GoalPoints;
-        }
-        public bool IsCompleted() {return _isCompleted;}
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using System;
+
+// namespace Develop05;
+
+// public class Goal
+// {
+//     private string _goalName;
+//     private string _goalDescription; 
+//     protected int _goalPoints;
+
+//     public string GetGoalName()
+//     {
+//         return _goalName;
+//     }
+
+//     public void SetGoalName(string goalName)
+//     {
+//         _goalName = goalName;
+//     }
+
+//     public string GetDescription()
+//     {
+//         return _goalDescription;
+//     }
+
+//     public void SetGoalDescription(string goalDescription)
+//     {
+//         _goalDescription = goalDescription;
+//     }
+
+//     public int GetPoints()
+//     {
+//         return _goalPoints;
+//     }
+
+//     public void SetPoints(int goalPoints)
+//     {
+//         _goalPoints = goalPoints;
+//     }
+
+//     virtual int AddPoints(int totalPoints, int pointsEarned)
+//     {
+//         return totalPoints + pointsEarned;
+//     }
+// }
--- Eternal Quest/Simple.cs
+++ Eternal Quest/Simple.cs
@@ -5,19 +5,54 @@ namespace Develop05;
 public class Simple : Goal
 {
     // private int _goalPoints;
+    private string _cwgoalName;
+    private string _cwgoalDescription;
+    private int _cwgoalPoints;
     
-    // public void SetPoints(int goalPoints)
-    // {
-    //     _goalPoints = goalPoints;
-    // }
-    override int AddPoints(int totalPoints, int pointsEarned)
+    public void SetVariables(string cwgoalName, string cwgoalDescription, int cwgoalPoints)
     {
-        return totalPoints + pointsEarned;
+        _cwgoalName = cwgoalName;
+        _cwgoalDescription = cwgoalDescription;
+        _cwgoalPoints = cwgoalPoints;
     }
 
-    override int RecordEvent()
+    public string GetVariables()
     {
-        //
+        string _cwwrittenPoints = _cwgoalPoints.ToString();
+        return $"{_cwgoalName}|{_cwgoalDescription}|{_cwwrittenPoints}";
+    }
+    protected override int AddPoints(int cwtotalPoints, int cwpointsEarned)
+    {
+        return cwtotalPoints + cwpointsEarned;
+    }
+
+    // Originally: protected override int RecordEvent(string cwfileName, string userGoal)  //, string userGoal
+    protected override int RecordEvent(string cwuserChoice, string cwfileName)
+    {
+        string[] cweventParts = cwuserChoice.Split("|");
+        string _cwgType = cweventParts[0];
+        string _cwgName = cweventParts[1];
+        string _cwgDescription = cweventParts[2];
+        string _cwgPoints = cweventParts[3];
+        int _cwpoints = int.Parse(_cwgPoints);
+        string _cwgComplete = cweventParts[4];
+        bool _cwcomplete = bool.Parse(_cwgComplete);
+
+        return _cwpoints;
+    }
+
+    protected bool IsComplete(string cwuserChoice, string cwfileName)
+    {
+        string[] cweventParts = cwuserChoice.Split("|");
+        string _cwgType = cweventParts[0];
+        string _cwgName = cweventParts[1];
+        string _cwgDescription = cweventParts[2];
+        string _cwgPoints = cweventParts[3];
+        int _cwpoints = int.Parse(_cwgPoints);
+        string _cwgComplete = cweventParts[4];
+        bool _cwcomplete = bool.Parse(_cwgComplete);
+
+        return _cwcomplete;
     }
 
 }
